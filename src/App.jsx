487964688173f27e1de1ad8a559da1d9055014cb
sunnytanyusha <sunnytanyusha@gmail.com>
2024-03-01 22:42:09 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Button from './components/Button'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Vite + React</h1>
+      <Button 
+        text="My button" 
+        placeholder="Some placeholder"
+        />
+    </div>
+  )
+}
+
+export default App
